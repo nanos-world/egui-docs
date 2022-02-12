35b2a5f6aa7142eeee318b0fc744c9c59ff1b8cc
nanosbot <nanosbot-github@nanos.io>
2022-02-12 21:23:17 +0000

    deploy: 3e2d910f5470ba3d1233f5182d0be69b4a3c31a7

---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'EGUI Documentation',
+  tagline: 'EGUI Documentation',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'nanos-world', // Usually your GitHub org/user name.
+  projectName: 'egui-docs', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/nanos-world/egui-docs/edit/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'EGUI',
+        items: [
+          {
+            type: 'doc',
+            docId: 'getting-started',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: [
+          'lua',
+        ],
+      },
+    }),
+};
+
+module.exports = config;
